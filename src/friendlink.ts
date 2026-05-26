1580b25832f29037d3d9e1a63bc2958b103d8714
XVVQAQ <XVVQAQ143352@outlook.com>
2026-05-27 00:05:23 +0800

    feat(friendlink): add initial friend link entry for PTALTS-LK

--- src/friendlink.ts
+++ src/friendlink.ts
@@ -7,4 +7,12 @@ export interface FriendLink {
   hoverDescription?: string;
 }
 
-export const friendLinks: FriendLink[] = [{}];
+export const friendLinks: FriendLink[] = [
+  {
+    avatar: "https://avatars.githubusercontent.com/u/62936489",
+    name: "PTALTS-LK",
+    description: "PTALTS-LK的博客",
+    url: "https://ptalts-lk.github.io/",
+    hoverDescription: "手台领域大蛇(x)",
+  },
+];
